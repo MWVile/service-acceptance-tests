72b193b0a2ab20d21815e94c4b4acb15cea6caab
Andy <andy@AT233881.local>
2020-07-17 15:36:09 +0100

    fixed path

--- features/step-definitions/steps-webtest/webtest.rb
+++ features/step-definitions/steps-webtest/webtest.rb
@@ -3,7 +3,7 @@
 # Use Capybara "Visit" method to visit the webpage
 # provided by the "path" variable
 Given('I visit Awesomeness UI') do ||
-  visit(Env.flask_skeletonxl_ui_url)
+  visit(Env.flask_skeletonxl_ui_url + "/awesomeness")
 end
 Then('the text {string} is displayed on the page') do |search_text|
   # Use RSPEC assertion to make sure text from variable 'search_text'
