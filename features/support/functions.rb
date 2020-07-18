9b5629f3621a99bab75165cd4a396e736df56532
Dylan Piergies <dylan.piergies@landregistry.gov.uk>
2020-07-18 19:55:33 +0100

    Print URLs being requested

--- features/support/functions.rb
+++ features/support/functions.rb
@@ -1,5 +1,6 @@
 def http_get_response_code(check_url)
   url = URI.parse(check_url)
+  print "Making GET request for #{url}"
   req = Net::HTTP.new(url.host, url.port)
   url.path = "/" if url.path == ""
   req.use_ssl = true if url.scheme == 'https'
