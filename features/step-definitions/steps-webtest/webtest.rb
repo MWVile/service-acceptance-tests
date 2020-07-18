2428367a9cda5cbd678be1c818e88cc1aad66f80
Dylan Piergies <dylan.piergies@landregistry.gov.uk>
2020-07-18 17:07:35 +0100

    Remove CXF path from JAX-RS service base URL

--- features/step-definitions/steps-webtest/webtest.rb
+++ features/step-definitions/steps-webtest/webtest.rb
@@ -3,7 +3,7 @@
 # Use Capybara "Visit" method to visit the webpage
 # provided by the "path" variable
 Given('I visit Awesomeness UI') do ||
-  visit(Env.flask_skeletonxl_ui_url + "/awesomeness")
+  visit(Env.flask_skeletonxl_ui_url + '/awesomeness')
 end
 Then('the text {string} is displayed on the page') do |search_text|
   # Use RSPEC assertion to make sure text from variable 'search_text'
@@ -11,35 +11,35 @@ Then('the text {string} is displayed on the page') do |search_text|
   expect(page).to have_content(search_text)
 end
 
-When("I enter the first name of {string}") do |firstname|
-  fill_in("forename", with: firstname)
+When('I enter the first name of {string}') do |firstname|
+  fill_in('forename', with: firstname)
 end
 
-When("I enter the surname name of {string}") do |surname|
-  fill_in("surname", with: surname)
+When('I enter the surname name of {string}') do |surname|
+  fill_in('surname', with: surname)
 end
 
-When("I click continue") do
+When('I click continue') do
   click_button('submit_button')
 end
 
 
 Given('I check Awesomeness UI') do ||
   code = http_get_response_code(Env.flask_skeletonxl_ui_url + '/health')
-  expect(code).to eq("200")
+  expect(code).to eq('200')
 end
 
 Given('I check Awesomeness API') do ||
   code = http_get_response_code(Env.flask_skeletonxl_api_url + '/health')
-  expect(code).to eq("200")
+  expect(code).to eq('200')
 end
 
 Given('I check Awesomeness Jaxrs') do ||
   code = http_get_response_code(Env.sb_jaxrs_skeletonxl_api_url + '/actuator/health')
-  expect(code).to eq("200")
+  expect(code).to eq('200')
 end
 
 Given('I check Awesomeness SB MVC') do ||
   code = http_get_response_code(Env.sb_mvc_skeletonxl_api_url + '/actuator/health')
-  expect(code).to eq("200")
+  expect(code).to eq('200')
 end
