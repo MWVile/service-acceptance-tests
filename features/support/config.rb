b6005c902a9e604d8d10f2971ba5525b05844092
Simon Chapman <simon.chapman@landregistry.gov.uk>
2020-08-19 12:09:02 +0100

    Ignore cert errors (for local running against self-signed)

--- features/support/config.rb
+++ features/support/config.rb
@@ -39,6 +39,7 @@ Capybara.register_driver :chrome do |app|
   options.add_argument('disable-gpu')
   options.add_argument('window-size=1920,1080')
   options.add_argument('disable-dev-shm-usage')
+  options.add_argument('ignore-certificate-errors')
 
   Capybara::Selenium::Driver.new(app,
                                  browser: :chrome,
--- features/support/functions.rb
+++ features/support/functions.rb
@@ -4,6 +4,7 @@ def http_get_response_code(check_url)
   req = Net::HTTP.new(url.host, url.port)
   url.path = "/" if url.path == ""
   req.use_ssl = true if url.scheme == 'https'
+  req.verify_mode = OpenSSL::SSL::VERIFY_NONE if url.scheme == 'https'
   res = req.request_get(url.path)
   return res.code
 end
